01e6d2e2ce78cf59103c1e87b28e59dcfac8e85e
Shoaib-Khalid-11 <shoaibkhalid3300@gmail.com>
2026-05-21 16:03:53 +0500

    create a random string

---
+++ backed_data_sourses/random.ts
@@ -0,0 +1,5 @@
+// node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
+
+// create for random string
+
+// export const randomString = () => require('crypto').randomBytes(32).toString('hex');
